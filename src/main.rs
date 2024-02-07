c390136022b99ed7998d4df68bff71b18a51918a
David Yeboah <daveyeb@gmail.com>
2024-02-07 17:35:47 -0500

    done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use crate::enigma::Enigma;
+
 mod rotors;
 mod circuit;
 mod enigma;
 mod util;
 
 fn main() {
-    println!("Hello, world!");
+    let plugboard = [('X', 'S'), ('F', 'V')];
+
+    let mut enigma = Enigma::from_config(&["i", "ii", "iii"], "ukwc", None, Some(&plugboard));
+
+    println!("{}", enigma.encode("EPAUK PEDJERBYLE HIIXJ MQVLJDNCYF BYLUZ QMVGOVQXXC JFGOF CWJEEMPWUZ GFWLF ZMUBHZRWXQ GDAWH KCIAUIJSWO CGSRH NRFKQPDHLT QYLBE FRGMLQEJMB NOUYD JBYITCVNMM KGGIP VYMVGSCFHN"))
 }
